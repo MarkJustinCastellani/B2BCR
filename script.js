922f728f45c38d16100cfabbbbaaa872f4260936
MarkJustinCastellani <135801188+MarkJustinCastellani@users.noreply.github.com>
2023-06-09 03:15:19 -0700

    script.js

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 function initializeApp() {
-
   // Add your code for initializing the application here
-
   addRestaurant("Restaurant 1", "Address 1", "Contact 1");
-
   addCook("Cook 1", "Specialization 1", "Contact 1");
 
   displayRestaurants();
-
   displayCooks();
-
 }
 
 function addRestaurant(name, address, contact) {
-
   // Add code to add a new restaurant to your application
-
 }
 
 function addCook(name, specialization, contact) {
-
   // Add code to add a new cook to your application
-
 }
 
 function displayRestaurants() {
-
   // Add code to display the list of restaurants in your application
-
 }
 
 function displayCooks() {
-
   // Add code to display the list of cooks in your application
-
 }
 
 initializeApp(); // Call the initializeApp function to start the application
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
 
