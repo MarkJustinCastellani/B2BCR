25a73d45e0c97dd701cd860b7ac6777989b7972d
MarkJustinCastellani <135801188+MarkJustinCastellani@users.noreply.github.com>
2023-06-11 00:33:17 -0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// Declare variables
+var itemCount = 0;
+let maxItemCount = 10;
+const minItemCount = 0;
+
+// Add your code for initializing the application here
 function initializeApp() {
-  // Add your code for initializing the application here
   addRestaurant("Restaurant 1", "Address 1", "Contact 1");
   addCook("Cook 1", "Specialization 1", "Contact 1");
 
@@ -7,23 +12,32 @@ function initializeApp() {
   displayCooks();
 }
 
+// Add code to add a new restaurant to your application
 function addRestaurant(name, address, contact) {
-  // Add code to add a new restaurant to your application
+  // Here you can write the code to add a new restaurant to the application
+  console.log("Adding a new restaurant:", name, address, contact);
 }
 
+// Add code to add a new cook to your application
 function addCook(name, specialization, contact) {
-  // Add code to add a new cook to your application
+  // Here you can write the code to add a new cook to the application
+  console.log("Adding a new cook:", name, specialization, contact);
 }
 
+// Add code to display the list of restaurants in your application
 function displayRestaurants() {
-  // Add code to display the list of restaurants in your application
+  console.log("Displaying list of restaurants");
+  // Here you can write the code to display the list of restaurants
 }
 
+// Add code to display the list of cooks in your application
 function displayCooks() {
-  // Add code to display the list of cooks in your application
+  console.log("Displaying list of cooks");
+  // Here you can write the code to display the list of cooks
 }
 
-initializeApp(); // Call the initializeApp function to start the application
+// Call the initializeApp function to start the application
+initializeApp();
 
 
 
@@ -52,6 +66,17 @@ initializeApp(); // Call the initializeApp function to start the application
 
 
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
 
 
